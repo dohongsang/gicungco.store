1ee8c791a6590816bac411290866ad7f63622813
Sang <Kenny@admins-MacBook-Air.local>
2020-03-18 15:47:46 +0700

    new deploy

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import Layout from '../components/MyLayout.js'
-
 export default function About() {
   return (
-    <Layout>
+    <div>
       <p>This is the about page</p>
-    </Layout>
+    </div>
   )
 }
